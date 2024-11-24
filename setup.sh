66648c0abba7a48deceb3665ce87f08adb05fc04
juniorsir <140545024+juniorsir@users.noreply.github.com>
2024-11-24 19:21:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo"Python is InSTallinG"
+echo "Python is InSTallinG"
 
 pkg install python
 
-echo"flask Is iNstaLLing"
+echo "flask Is iNstaLLing"
 pip install flast
 
-echo"Coping nacessary file"
+echo "Coping nacessary file"
 
 wget https://raw.githubusercontent.com/juniorsir/bloggerpost-/main/app.py
 wget https://raw.githubusercontent.com/juniorsir/bloggerpost-/main/preview.html
 wget https://raw.githubusercontent.com/juniorsir/bloggerpost-/main/form.html
 
-echo"Setup in Progress"
+echo "Setup in Progress"
 
 mkdir bloggerpost
 mkdir bloggerpost/tamplates
@@ -22,4 +22,4 @@ mv form.html bloggerpost/templates
 mv preview.html bloggerpost/templates
 cd bloggerpost
 clear
-echo"Now You can use python app.py to launch web server"
+echo "Now You can use python app.py to launch web server"
