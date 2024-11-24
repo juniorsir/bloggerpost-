4f57bfe3cfc3875131dda58073da128cd388e4b5
juniorsir <140545024+juniorsir@users.noreply.github.com>
2024-11-24 19:00:17 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo"Python is InSTallinG"
+
+pkg install python
+
+echo'flask Is iNstaLLing"
+pip install flast
+
+mkdir bloggerpost
+mkdir bloggerpost/tamplates
