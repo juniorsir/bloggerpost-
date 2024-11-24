016b03fbbfa4abd34f0f5295887468ceee00260e
juniorsir <140545024+juniorsir@users.noreply.github.com>
2024-11-24 19:05:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,22 @@ echo"Python is InSTallinG"
 
 pkg install python
 
-echo'flask Is iNstaLLing"
+echo"flask Is iNstaLLing"
 pip install flast
+
+echo"Coping nacessary file"
+
+wget https://github.com/juniorsir/bloggerpost-/blob/main/preview.html
+wget https://github.com/juniorsir/bloggerpost-/blob/main/form.html
+wget https://github.com/juniorsir/bloggerpost-/blob/main/app.py
+
+echo"Setup in Progress"
 
 mkdir bloggerpost
 mkdir bloggerpost/tamplates
+mv app.py bloggerpost
+mv form.html bloggerpost/templates
+mv preview.html bloggerpost/templates
+cd bloggerpost
+clear
+echo"Now You can use python app.py to launch web server"
